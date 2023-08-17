3beba06a3d18b3cf8a31aa61e513a541a65b290e
Replit user <>
2023-08-17 20:46:16 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -68,10 +68,10 @@ int checkSorted(int arr[], int size)
   for(int i = 0 ; i < size ; i++)
     {
       if(arr[i] > arr[i + 1])
-        return 1;
+        return 0;
       
     }
-  return 0;
+  return 1;
 }
 
 /*
