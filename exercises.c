28a9c61e55c1205930b82ef6d487c3ab4e16623f
Replit user <>
2023-08-18 20:27:33 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -135,7 +135,6 @@ int checkSorted(int arr[], int size)
         descendente = 0;
       }
     }
-
   if (ascendente)
   {
     return 1;
@@ -172,7 +171,13 @@ typedef struct {
 } Libro;
 
 void inicializarLibro(Libro *libro, const char *titulo, const char *nombreAutor,
-                      int anioNacimiento, int anioPublicacion) {}
+                        int anioNacimiento, int anioPublicacion)
+{
+  strcpy(libro->titulo, titulo);
+  strcpy(libro->autor.nombre, nombreAutor);
+  libro->autor.anioNacimiento = anioNacimiento;
+  libro->anioPublicacion =  anioPublicacion;
+}
 
 /*
 Ejercicio 7: Lista enlazada de números
